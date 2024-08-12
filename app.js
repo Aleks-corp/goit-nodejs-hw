4ed47d62ed2c3de73d339e70d0bd5de38e797e62
Aleks Haran <101347633+Aleks-corp@users.noreply.github.com>
2024-08-12 12:56:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-import express from 'express';
-import logger from 'morgan';
-import cors from 'cors';
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
 
-import contactsRouter from './routes/api/contacts.js';
-import authRouter from './routes/api/auth.js';
+import contactsRouter from "./routes/api/contacts.js";
+import authRouter from "./routes/api/auth.js";
 
 const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use('/users', authRouter);
-app.use('/api/contacts', contactsRouter);
+app.use("/users", authRouter);
+app.use("/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' });
+  res.status(404).json({ message: "Not found" });
 });
 
 app.use((error, req, res, next) => {
